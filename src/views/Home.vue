df25de94d195b864cacc56f49c5c6bbcdcdcae4a
zhengbifeng <zhengbifeng@hzmc.com.cn>
2023-10-25 15:26:47 +0800

    type: feat 调整首页H5,增加el-card的滚动效果

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,71 @@
 <template>
   <div class="container">
-    <div class="option-row-container">
-      <div class="option-wrapper" @click="goTo('shop')">
-        <el-card class="option">
-          <a class="card-image font-neon">管理后台</a>
-        </el-card>
-      </div>
+    <div class="carousel-container">
+      <div class="carousel-container-ul">
+        <!-- a. 向前的图标 -->
+        <i class="el-icon-arrow-left carousel-arrow" @click="scroll(-1)"></i>
 
-      <div class="option-wrapper" @click="goTo('blog')">
-        <el-card class="option">
-          <a class="card-image font-transparent">个人博客</a>
-        </el-card>
+        <!-- b. 中间待滚动card -->
+        <div class="options-container">
+          <ul ref="carousel">
+            <li>
+              <div class="option-wrapper" @click="goTo('shop')">
+                <el-card class="option">
+                  <div class="card-label">项目</div>
+                  <a class="card-image font-neon">管理后台</a>
+                </el-card>
+              </div>
+            </li>
+            <li>
+              <div class="option-wrapper" @click="goTo('blog')">
+                <el-card class="option">
+                  <div class="card-label">博客</div>
+                  <a class="card-image font-transparent">个人博客</a>
+                </el-card>
+              </div>
+            </li>
+            <li>
+              <div class="option-wrapper" @click="goTo('flask')">
+                <el-card class="option">
+                  <div class="card-label">接口</div>
+                  <a class="card-image card-swagger font-huerotate">Flask</a>
+                </el-card>
+              </div>
+            </li>
+            <li>
+              <div class="option-wrapper" @click="goTo('django')">
+                <el-card class="option">
+                  <div class="card-label">接口</div>
+                  <a class="card-image card-swagger font-magic">Django</a>
+                </el-card>
+              </div>
+            </li>
+            <li>
+              <div class="option-wrapper" @click="goTo('gin')">
+                <el-card class="option">
+                  <div class="card-label">接口</div>
+                  <a class="card-image card-swagger font-huerotate">Gin</a>
+                </el-card>
+              </div>
+            </li>
+          </ul>
+        </div>
+
+        <!-- c. 向后的图标 -->
+        <i class="el-icon-arrow-right carousel-arrow" @click="scroll(1)"></i>
       </div>
     </div>
-    <div class="option-row-container">
-      <div class="option-wrapper" @click="goTo('FlaskSwagger')">
-        <el-card class="option">
-          <a class="card-image card-swagger font-huerotate">FlaskSwagger</a>
-        </el-card>
-      </div>
-      <div class="option-wrapper" @click="goTo('GinSwagger')">
-        <el-card class="option">
-          <a class="card-image card-swagger font-magic">GinSwagger</a>
-        </el-card>
-      </div>
-    </div>
+
     <div class="footer-container">
-      <!-- Footer with record information -->
-      <footer style="text-align: center; margin-top: 20px; color: white">
-        <p><a href="https://beian.miit.gov.cn/" target="_blank">浙ICP备18007284号-1</a></p>
+      <footer>
+        <p>
+          <a href="https://beian.mps.gov.cn/#/query/webSearch" target="_blank">
+            <img :src="require('@/assets/img/tb1.png')" alt="" style="width: 20px" />
+          </a>
+          <a href="https://beian.miit.gov.cn/" target="_blank" style="color: white; font-size: 20px">
+            浙ICP备18007284号-1
+          </a>
+        </p>
       </footer>
     </div>
   </div>
@@ -36,37 +73,73 @@
 
 <script>
 export default {
+  data() {
+    return {
+      autoScrollInterval: null,
+      currentScrollIndex: 0,
+      maxScrollIndex: 2,
+    };
+  },
   methods: {
     goTo(destination) {
-      console.log(destination);
       if (destination == "shop") {
-        window.location.href = process.env.VUE_APP_SHOP_HOST;
+        // window.location.href = process.env.VUE_APP_SHOP_HOST;
+        window.open(process.env.VUE_APP_SHOP_HOST, "_blank");
       } else if (destination == "blog") {
-        window.location.href = process.env.VUE_APP_BLOG_HOST;
-      } else if (destination == "FlaskSwagger") {
-        window.location.href = process.env.VUE_APP_SHOP_FLASK_HOST;
-      } else if (destination == "GinSwagger") {
-        window.location.href = process.env.VUE_APP_SHOP_GIN_HOST;
+        // window.location.href = process.env.VUE_APP_BLOG_HOST;
+        window.open(process.env.VUE_APP_BLOG_HOST, "_blank");
+      } else if (destination == "flask") {
+        // window.location.href = process.env.VUE_APP_SHOP_FLASK_HOST;
+        window.open(process.env.VUE_APP_SHOP_FLASK_HOST, "_blank");
+      } else if (destination == "gin") {
+        // window.location.href = process.env.VUE_APP_SHOP_GIN_HOST;
+        window.open(process.env.VUE_APP_SHOP_GIN_HOST, "_blank");
+      } else if (destination == "django") {
+        // window.location.href = process.env.VUE_APP_SHOP_DJANGO_HOST;
+        window.open(process.env.VUE_APP_SHOP_DJANGO_HOST, "_blank");
+      } else {
+        window.open(process.env.VUE_APP_SHOP_DJANGO_HOST, "_blank");
       }
     },
+    /* 根据direction来决定移动 */
+    scroll(direction) {
+      const carousel = this.$refs.carousel;
+      const scrollAmount = 330;
+      const step = direction * 10; // 每一帧滚动的距离
+      const frames = scrollAmount / step; // 需要的帧数
+      let frameCount = 0; // 当前帧数
+
+      /* 添加动画效果 */
+      const animateScroll = () => {
+        carousel.scrollLeft += step;
+        frameCount++;
+        if (frameCount < frames) {
+          requestAnimationFrame(animateScroll);
+        }
+      };
+      requestAnimationFrame(animateScroll);
+      // carousel.scrollLeft += scrollAmount * direction;
+      this.currentScrollIndex = carousel.scrollLeft / scrollAmount;
+    },
+  },
+  mounted() {
+    this.autoScrollInterval = setInterval(() => {
+      if (this.currentScrollIndex >= this.maxScrollIndex) {
+        const carousel = this.$refs.carousel;
+        this.currentScrollIndex = 0;
+        carousel.scrollLeft = 0;
+      } else {
+        this.scroll(1);
+      }
+    }, 5000);
+  },
+  beforeDestory() {
+    clearInterval(this.autoScrollInterval);
   },
 };
 </script>
 
 <style scoped>
-.container {
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  padding: 0 20%;
-  background-image: url(~@/assets/img/bg.jpg);
-  background-size: 100%;
-}
-
-.footer-container {
-  padding-bottom: 5px;
-}
-
 a {
   text-decoration: none; /* 移除下划线 */
   color: inherit; /* 使链接颜色与其父元素相同 */
@@ -75,28 +148,64 @@ a {
   font-style: inherit; /* 防止斜体样式 */
   font-weight: inherit; /* 防止加粗 */
 }
+ul,
+li {
+  margin: 0;
+  padding: 0;
+  list-style-type: none; /* 移除默认的列表标记（例如，圆点） */
+}
 
-.option-row-container {
-  height: 50%;
-  padding: 1rem;
-  cursor: pointer;
-
+.container {
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: space-between; /* 将主轴的空间分布在容器的两个子元素之间 */
+
+  background-image: url(~@/assets/img/bg.jpg);
+  background-size: 100%;
+}
+
+.carousel-container {
+  margin: 10px auto;
+}
+
+.carousel-container-ul {
+  display: flex;
   align-items: center;
 
-  border-bottom: 1px solid black;
+  overflow: hidden;
+  margin: 10px auto;
+  overflow: hidden; /* 隐藏滚动条 */
+  white-space: nowrap; /* 确保内容不换行 */
+}
+
+.carousel-arrow {
+  cursor: pointer;
+  font-size: 64px;
+  padding: 0 10px;
+  color: white;
+  z-index: 1;
+}
+
+.options-container {
+  margin-top: 100px;
+  width: 990px;
+}
+
+.options-container ul {
+  display: flex;
+  flex-wrap: nowrap; /* 确保内容不换行 */
+  overflow-x: hidden; /* 隐藏滚动条 */
 }
 
 .option-wrapper {
   transition: all 0.3s;
   border-radius: 5px;
+  height: 60vh;
   overflow: hidden;
 
-  /* 居中 */
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  width: 330px; /* 设置固定宽度 */
+  display: inline-block; /* 让内容水平排列 */
 }
 
 .option-wrapper:hover {
@@ -105,11 +214,23 @@ a {
 
 .option {
   /* 将el-card背景设置为透明 */
-  background-color: transparent;
+  background: rgba(255, 255, 255, 0.1);
 
-  /* 删除边框 */
-  border: none;
+  /* 删除el-card默认的边框 */
   box-shadow: none;
+  border: none;
+  position: relative;
+
+  margin: 10px;
+
+  /* 居中 */
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 100%;
+  text-align: center;
 }
 
 .option .card-image {
@@ -124,20 +245,25 @@ a {
   font-size: 60px;
 }
 
+.footer-container {
+  padding-bottom: 20px;
+  margin-bottom: 20px;
+  text-align: center;
+}
+
 /* 透明背景, 只保留边框 */
 .font-transparent {
   /* 设置背景 */
   color: transparent;
   /* 设置字体的边框 */
-  -webkit-text-stroke: 1px #fff;
-  text-stroke: 1px #fff;
+  -webkit-text-stroke: 3px #6d2901;
+  text-stroke: 3px #6d2901;
 }
 
 /* 发光字体 */
 .font-neon {
   color: #fff;
   text-shadow: 0 0 10px #0ebeff, 0 0 20px #0ebeff, 0 0 50px #0ebeff, 0 0 100px #0ebeff, 0 0 200px #0ebeff;
-  padding: 10px;
   display: inline-block;
   background-color: rgba(0, 0, 0, 0.4); /* 添加半透明的黑色背景 */
   border-radius: 8px; /* 为背景添加圆角 */
@@ -173,5 +299,23 @@ a {
 
 .border-right {
   border-right: 1px solid black;
+}
+
+.card-label {
+  position: absolute;
+  top: 10px; /* 调整为所需的位置 */
+  left: 10px; /* 调整为所需的位置 */
+  color: #ab6e35;
+
+  width: 80px;
+  height: 80px;
+  border-radius: 50%; /* 使其为圆形 */
+  font-size: 32px; /* 字体大小 */
+  background-color: rgba(249, 249, 249, 0.5); /* #f9f9f9 */
+
+  /* 居中 */
+  display: flex;
+  justify-content: center; /* 水平居中 */
+  align-items: center; /* 垂直居中 */
 }
 </style>
