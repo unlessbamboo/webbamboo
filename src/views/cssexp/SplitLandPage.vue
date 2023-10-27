ee77d01040f4a3df37cc989926ab3efc2c0539ae
zhengbifeng <zhengbifeng@hzmc.com.cn>
2023-10-27 09:31:36 +0800

    type: feat 分割页面改动:将两屏分为三屏

--- src/views/cssexp/SplitLandPage.vue
+++ src/views/cssexp/SplitLandPage.vue
@@ -2,20 +2,30 @@
   <div class="csscontainer-wrapper">
     <div class="csscontainer" ref="csscontainer">
       <div class="split left" @mouseenter="splitEnterLeft" @mouseleave="splitLeaveLeft">
-        <h1>PlayStation 5</h1>
+        <h2>PlayStation 5</h2>
         <a href="#" class="btn">Buy Now</a>
       </div>
 
-      <div class="split right" @mouseenter="splitEnterRight" @mouseleave="splitLeaveRight">
+      <div class="split center" @mouseenter="splitEnterCenter" @mouseleave="splitLeaveCenter">
         <h3>分割页面随鼠标移动测试页面</h3>
         <div class="page-content">
           <p>这是一个测试div宽度跟随鼠标进行移动动画的测试页面.</p>
           <ul>
-            <li>a. css的变量定义在多个vue页面中不能定义相同变量, 例如:root, 否则可能会产生冲突</li>
-            <li>b. 按钮使用absolute进行居中定位确保里面的a链接标签居中</li>
-            <li>c. 左右两个页面通过before伪元素设置一个100%高宽的背景色</li>
+            <li>css的变量定义在多个vue页面中不能定义相同变量, 例如:root, 否则可能会产生冲突</li>
+            <li>按钮使用absolute进行居中定位确保里面的a链接标签居中</li>
+            <li>左右两个页面通过before伪元素设置一个100%高宽的背景色</li>
           </ul>
+
+          <h4 id="改动" style="color: red">改动</h4>
+          <ol>
+            <li>将两屏分割更改为三屏, 同时调整中间的绝对定位的left值确保能够另外两屏能够联动调整</li>
+          </ol>
         </div>
+      </div>
+
+      <div class="split right" @mouseenter="splitEnterRight" @mouseleave="splitLeaveRight">
+        <h2>Genshin Impact</h2>
+        <a href="#" class="btn">Go To</a>
       </div>
     </div>
   </div>
@@ -31,6 +41,12 @@ export default {
     splitLeaveLeft() {
       this.$refs.csscontainer.classList.remove("hover-left");
     },
+    splitEnterCenter() {
+      this.$refs.csscontainer.classList.add("hover-center");
+    },
+    splitLeaveCenter() {
+      this.$refs.csscontainer.classList.remove("hover-center");
+    },
     splitEnterRight() {
       this.$refs.csscontainer.classList.add("hover-right");
     },
@@ -42,7 +58,7 @@ export default {
 </script>
 
 <style scoped>
-h1,
+h2,
 h3 {
   color: #fff;
   position: absolute;
@@ -52,8 +68,8 @@ h3 {
   white-space: nowrap;
 }
 
-h1 {
-  font-size: 4rem;
+h2 {
+  font-size: 3rem;
 }
 
 h3 {
@@ -67,14 +83,16 @@ h3 {
 
   --left-bg-color: rgba(87, 84, 236, 0.7);
   --right-bg-color: rgba(43, 43, 43, 0.8);
+  --center-bg-color: rgba(125, 43, 43, 0.8);
   --left-btn-hover-color: rgba(87, 84, 236, 1);
   --right-btn-hover-color: rgba(28, 122, 28, 1);
-  --hover-width: 80%;
+  --hover-width: 60%;
   --minimize-width: 20%;
   --transition-speed: 1s;
 }
 
 .csscontainer {
+  /* 通过relative确保子元素能够绝对定位进行居中处理 */
   position: relative;
   width: 100%;
   height: 100%;
@@ -98,21 +116,25 @@ h3 {
 }
 
 .btn {
-  color: #fff;
-  position: absolute;
+  /* 确保btn后续的子节点居中 */
   display: flex;
   align-items: center;
   justify-content: center;
+
+  /* btn自身的居中 */
+  position: absolute;
   left: 50%;
   top: 40%;
   transform: translateX(-50%);
+
+  color: #fff;
   border: #fff solid 0.2rem;
-  text-decoration: none;
+  padding: 1.5rem;
   font-size: 1rem;
   font-weight: bold;
-  text-transform: uppercase;
   width: 15rem;
-  padding: 1.5rem;
+  text-decoration: none;
+  text-transform: uppercase;
 }
 
 .split.left .btn:hover {
@@ -125,17 +147,22 @@ h3 {
   border-color: var(--right-btn-hover-color);
 }
 
+.split.center .btn:hover {
+  background-color: var(--right-btn-hover-color);
+  border-color: var(--right-btn-hover-color);
+}
+
+/* 通过设置50%宽度, 从而让左右两边各占一半屏幕 */
 .split {
   position: absolute;
-  width: 50%;
+  width: 33.33%;
   height: 100%;
   overflow: hidden;
 }
 
 .split.left {
   left: 0;
-  background: url("https://images.unsplash.com/photo-1606144042614-b2417e99c4e3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80")
-    no-repeat center / cover;
+  /* background: url("") no-repeat center / cover; */
 }
 
 .split.left::before {
@@ -143,13 +170,24 @@ h3 {
   position: absolute;
   width: 100%;
   height: 100%;
-  background-color: rgba(87, 84, 236, 0.7);
+  background-color: var(--left-bg-color);
+}
+
+.split.center {
+  left: 33.33%;
+}
+
+.split.center::before {
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: var(--center-bg-color);
 }
 
 .split.right {
   right: 0;
-  background: url("https://images.unsplash.com/photo-1607853827120-6847830b38b0?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1762&q=80")
-    no-repeat center / cover;
+  /* background: url("") no-repeat center / cover; */
 }
 
 .split.right::before {
@@ -162,6 +200,8 @@ h3 {
 
 .split.left,
 .split.right,
+.split.center,
+.split.center::before,
 .split.left::before,
 .split.right::before {
   transition: all 1s ease-in-out;
@@ -169,26 +209,42 @@ h3 {
 
 .hover-left .left {
   width: var(--hover-width);
-  /* width: 75%; */
 }
 
-.hover-left .right {
+.hover-left .right,
+.hover-left .center {
   width: var(--minimize-width);
-  /* width: 25%; */
+}
+
+.hover-left .center {
+  left: 60%;
 }
 
 .hover-right .right {
   width: var(--hover-width);
-  /* width: 75%; */
 }
 
-.hover-right .left {
+.hover-right .left,
+.hover-right .center {
   width: var(--minimize-width);
-  /* width: 25%; */
+}
+
+.hover-right .center {
+  left: 20%;
+}
+
+.hover-center .center {
+  width: var(--hover-width);
+  left: 20%;
+}
+
+.hover-center .left,
+.hover-center .right {
+  width: var(--minimize-width);
 }
 
 @media (max-width: 800px) {
-  h1 {
+  h2 {
     font-size: 2rem;
     top: 30%;
   }
